45cc226ada47ec9c38435fbec4251d716485657d
httpFundaje <lopezjaderson@gmail.com>
2025-10-15 15:51:03 -0500

    Primer commit prueba

---
+++ src/main/java/GUI/mian.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author juane
+ */
+public class mian {
+    public static void main(String[] args) {
+        
+        Moto MOTO = new Moto();
+        MOTO.setVisible(true);
+        
+        
+        
+    }
+}
